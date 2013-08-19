cab9bcff756a2d9a0c629aaec95abe452dc7d047
Kjell <kjell@leanside.com>
2013-08-19 09:46:50 -0500

    Firebase to live update tiling status

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var http = require('http'),
     httpget = require('http-get'),
     express = require('express'),
     fs = require('fs'),
-    exec = require('exec')
+    exec = require('exec'),
+    firebase = require('firebase'),
     dx = require('./dx')
 
 app = express()
@@ -17,8 +18,10 @@ app.get('/:image', function(req, res) {
   tileJson = 'http://localhost:8888/v2/'+imageName+'.json'
   http.get(tileJson, function(tile_res) {
     console.log(req.params, tile_res.statusCode)
+    var progressRef = new firebase("https://tilesaw.firebaseio.com/"+imageName)
     if(tile_res.statusCode == '200') {
       tile_res.pipe(res) // pipe through the JSON from tilestream
+      progressRef.set({status: 'tiled'})
     } else {
       res.send(404) // return 404 then start the tiling process
       var size = dx.maxDimension(image)
@@ -30,13 +33,16 @@ app.get('/:image', function(req, res) {
         var tilesawPath = '/Users/kolsen/tmp',
             tilesaw = tilesawPath + '/tilesaw/tilesaw.sh';
 
+        progressRef.set({status: 'downloading original image'})
         httpget.get({url: imageUrl}, imageFile, function(error, result) {
+          progressRef.set({status: 'processing image'})
           console.log(result.file)
           var saw = exec([tilesaw, imageFile], function(err, out, code) {
             console.log('tilesaw', err, out, code)
             if(code == 0) {
               mv = exec(['mv', imageName + '.mbtiles', '/Users/kolsen/Documents/MapBox/tiles/'], function(err, out, code) {
                 exec(['rm', imageName + '.jpg'], function() {})
+                progressRef.set({status: 'tiled'})
               })
             } else {
               console.log('error')
