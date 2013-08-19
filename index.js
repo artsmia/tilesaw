0626a125b72f570aeec6434e2f4e2395a5863220
Kjell <kjell@leanside.com>
2013-08-19 12:35:05 -0500

    Dokku: Procfile; set paths and ports from ENV
    squash! f331b95d28b

--- index.js
+++ index.js
@@ -8,14 +8,20 @@ var http = require('http'),
 
 app = express()
 
+var tileserver = process.env.TILESERVER || 'http://localhost:8888/v2/'
+console.log('tileserver: ' + tileserver)
+
 app.get('/:image', function(req, res) {
   var image = req.params.image,
       imageName = image.replace(/\.\w+$/, '')
 
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET');
+  if(imageName == undefined) return
 
-  tileJson = 'http://localhost:8888/v2/'+imageName+'.json'
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET')
+
+  tileJson = tileserver+imageName+'.json'
+  console.log(' --- http.get ' + tileJson)
   http.get(tileJson, function(tile_res) {
     console.log(req.params, tile_res.statusCode)
     var progressRef = new firebase("https://tilesaw.firebaseio.com/"+imageName)
@@ -23,24 +29,28 @@ app.get('/:image', function(req, res) {
       tile_res.pipe(res) // pipe through the JSON from tilestream
       progressRef.set({status: 'tiled'})
     } else {
-      res.send(404) // return 404 then start the tiling process
-      var size = dx.maxDimension(image)
+      res.send(404) // return 404 then start the tiling process [TODO: use raw websockets instead of firebase?]
+      var size = dx.maxDimension(imageName)
+      console.log('image size :' + size)
 
       if(image != undefined) {
         var imageUrl = "http://api.artsmia.org/images/1/tdx/"+size+"/"+image
         console.log(imageUrl)
         var imageFile = imageName + '.jpg'
-        var tilesawPath = '/Users/kolsen/tmp',
-            tilesaw = tilesawPath + '/tilesaw/tilesaw.sh';
+        var tilesawPath = process.env.TILESAW_PATH || '/Users/kolsen/tmp/tilesaw',
+            tilesaw = tilesawPath + '/tilesaw.sh',
+            tileDirectory = process.env.TILE_DIRECTORY || '/Users/kolsen/Documents/Mapbox/tiles'
 
         progressRef.set({status: 'downloading original image'})
+        console.log('getting image: ' + imageUrl)
         httpget.get({url: imageUrl}, imageFile, function(error, result) {
+          console.log('got image')
           progressRef.set({status: 'processing image'})
           console.log(result.file)
           var saw = exec([tilesaw, imageFile], function(err, out, code) {
             console.log('tilesaw', err, out, code)
             if(code == 0) {
-              mv = exec(['mv', imageName + '.mbtiles', '/Users/kolsen/Documents/MapBox/tiles/'], function(err, out, code) {
+              mv = exec(['mv', imageName + '.mbtiles', tileDirectory], function(err, out, code) {
                 exec(['rm', imageName + '.jpg'], function() {})
                 progressRef.set({status: 'tiled'})
               })
@@ -54,4 +64,5 @@ app.get('/:image', function(req, res) {
   })
 })
 
-http.createServer(app).listen(8887);
+console.log('starting tilesaw on ' + (process.env.PORT || 8887))
+http.createServer(app).listen(process.env.PORT || 8887);
