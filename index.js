1dedca5324a37e4a46a66511286630ff177696d8
Kjell <kjell@leanside.com>
2014-02-18 12:59:37 -0600

    manifest: json listing all tiled images

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var http = require('http'),
     express = require('express'),
     exec = require('exec'),
     fs = require('fs'),
-    mkdirp = require('mkdirp')
+    mkdirp = require('mkdirp'),
+    async = require('async')
 
 app = express()
 
@@ -50,6 +51,34 @@ app.get('/:image', function(req, res) {
     }
   })
 })
+
+var manifest = {grouped: {}, all: []}
+var addToManifest = function(mbtiles, done) {
+  var filename = mbtiles.replace('.mbtiles', '')
+  manifest.all.push(filename)
+  // and manifest[objectId]
+  exec(['sqlite3', tiledirectory+mbtiles, "select * from metadata"], function(err, out, code) {
+    if(err) return done(err)
+    var metadata = out,
+        matchObjectId = out.match(/objectId\|(.*)/),
+        objectId = matchObjectId ? matchObjectId[1] : null
+
+    manifest.grouped[objectId] || (manifest.grouped[objectId] = [])
+    manifest.grouped[objectId].push(filename)
+    done()
+  })
+}
+fs.readdir(tiledirectory, function(err, files) {
+  if(err) return console.error("couldn't `fs.readdir` " + tiledirectory)
+
+  async.eachLimit(
+    files.filter(function(f) { return f.match(/mbtiles$/) }),
+    10,
+    addToManifest,
+    function(err) { console.log('manifest generated', err) }
+  )
+})
+app.get('/', function(req, res) { res.send(manifest) })
 
 http.createServer(app).listen(process.env.PORT || 8887);
 console.log('tilesaw running on ' + (process.env.PORT || 8887))
