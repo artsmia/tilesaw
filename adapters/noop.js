4a16e443c698317eea0e8f67cd97cec75906c0a9
Kjell <kjell@leanside.com>
2014-02-25 11:51:26 -0600

    'Stack' multiple adapters
    A series of adapters can be specified. For instance, first check in a
    directory to see if an image has been uploaded; if not, try to download
    it.
    
    Each adapter does a node style `fn(err, success)` callback. `err`
    queues up the next adapter.
    
    TODO: The tiling shouldn't happen in the adapter. In the case of
    multiple adapters (for instance 'local directory, mediabin') shouldn't
    the adapter only be responsible for grabbing an image? Then the tiling
    should happen after the image location has been determined.
    
    Unless maybe there's an adapter that finds pre-tiled images…

---
+++ adapters/noop.js
@@ -0,0 +1,4 @@
+module.exports = function(imageName, fn) {
+  console.log('noop adapter, noop`ing')
+  fn('noop')
+}
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app = express()
 
 var tileserver = process.env.TILESERVER || 'http://localhost:8888/v2/'
 
+var adapters = 'noop directory'
+
 app.get('/:image', function(req, res) {
   var image = req.params.image,
       imageName = image.replace(/\.\w+$/, '')
@@ -22,10 +24,17 @@ app.get('/:image', function(req, res) {
     if(tileRes.statusCode == '200') {
       tileRes.pipe(res) // pipe through the JSON from tilestream
     } else {
-      require('./adapters/directory')(imageName, function(err, mbtiles) {
-        if(err) return res.send(404, err)
+      var _adapters = adapters.split(' ')
+      _adapters.some(function(adapter, index) {
+        return require('./adapters/'+adapter)(imageName, function(err, mbtiles) {
+          if(err) {
+            if(index+1 == _adapters.length) res.send(404, err)
+            return false
+          }
 
-        http.get(tileJson, function(secondTileRes) { secondTileRes.pipe(res) })
+          http.get(tileJson, function(secondTileRes) { secondTileRes.pipe(res) })
+          return true
+        })
       })
     }
   })
