7cbc2c1ffe647c38d9f6dfd3763a2dcb951a76e8
Kjell <kjell@leanside.com>
2016-02-12 13:15:26 -0600

    quote filenames proper

--- tilesaw.sh
+++ tilesaw.sh
@@ -3,7 +3,7 @@
 
 image=$1
 
-if [ ! -f $image ]; then
+if [ ! -f "$image" ]; then
   echo "$image does not exist."
   exit 1
 fi
@@ -11,18 +11,19 @@ fi
 name="${image%.*}" # http://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
 extension="${image##*.}" # bash black magic
 
-tile_dir=tiles/$name
-mkdir -p $tile_dir
-rm -rf $tile_dir # make sure tiles/ exists, then clear tiles/$name
+tile_dir=tiles/"$name"
+mkdir -p "$tile_dir"
+rm -rf "$tile_dir" # make sure tiles/ exists, then clear tiles/$name
 
-vips dzsave $image $tile_dir --layout=google || { echo 'vips failed'; exit 1; }
+vips dzsave "$image" "$tile_dir" --layout=google || { echo 'vips failed'; exit 1; }
 
-metadata="{ \"name\": \"$name\",
-  \"width\": \"$(exiftool $image | grep Width | head -1 | cut -d ':' -f2 | tr -d ' ')\",
-  \"height\": \"$(exiftool $image | grep Height | head -1 | cut -d ':' -f2 | tr -d ' ')\",
-  \"objectId\": \"$(exiftool $image | grep Transmission | head -1 | tr -d 'Transmission Reference          :')\"
+metadata="{ \"name\": \"$(basename ${name})\",
+  \"width\": \"$(exiftool "$image" | grep Width | head -1 | cut -d ':' -f2 | tr -d ' ')\",
+  \"height\": \"$(exiftool "$image" | grep Height | head -1 | cut -d ':' -f2 | tr -d ' ')\",
+  \"objectId\": \"$(exiftool "$image" | grep Transmission | head -1 | tr -d 'Transmission Reference          :')\",
+  \"tiles\": [\"http://0.tiles.dx.artsmia.org/$(basename ${name})/{z}/{x}/{y}.jpg\"]
 }"
-echo $metadata > $tile_dir/metadata.json
+echo $metadata > "$tile_dir/metadata.json"
 
-mb-util $tile_dir/ $name.mbtiles --image_format=jpg 2> /dev/null
+mb-util "$tile_dir/" "$name".mbtiles --image_format=jpg 2> /dev/null
 echo
